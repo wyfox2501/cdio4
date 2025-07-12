b0b9ee2b7d9473747d2e9c0a34c13cffc58e45ce
Nguyen Hoang Huy <nhatnguyenhong45@gmail.com>
2025-07-13 02:07:53 +0700

    fix 11

--- client/src/features/doctor/DoctorPayment/DoctorPayment.jsx
+++ client/src/features/doctor/DoctorPayment/DoctorPayment.jsx
@@ -7,6 +7,7 @@ DoctorPayment.propTypes = {
 
 function DoctorPayment(props) {
     const [data, setData] = useState([]);
+    const [balance, setBalance] = useState([]);
     useEffect(() => {
         const fetchPayment= async () => {
             try {
@@ -23,15 +24,38 @@ function DoctorPayment(props) {
                 console.error('Error fetching payment data:', error);
             }
         }
+        const fetchCheck=async () => {
+            try {
+                const response = await fetch('http://localhost:5000/api/payment/check',{
+                    method: 'GET',
+                    credentials: 'include'
+                } );
+                if (!response.ok) {
+                    throw new Error('Network response was not ok');
+                }
+                const result = await response.json();
+                setBalance(result[0]);
+            } catch (error) {
+                console.error('Error fetching payment data:', error);
+            }
+        }
         fetchPayment();
+        fetchCheck();
     }, []);
+    
     return (
         <div className="doctor-payment">
-            <h2>Vietcombank</h2>
+        <div className="balance">
+            <h2>balance:</h2>
+            <p>{Number(balance.total_money)?.toLocaleString('vi-VN')} VND</p>
+        </div>
+          <div className="main">
+              <h2>Vietcombank</h2>
             <p><span>So tai khoan:</span> 0396603412</p>
             <p><span>Ten tai khoan:</span> Nguyen Hoang Huy</p>
             <p><span>Ngan hang:</span> Vietcombank</p>
             <img src={data.qrUrl} alt="QR MB" />
+          </div>
         </div>
     );
 }
--- server/routes/payment.js
+++ server/routes/payment.js
@@ -23,5 +23,18 @@ router.get("/create", async function(req, res, next) {
     return res.status(500).json({ message: "Internal server error" });
   }
 });
-
+// lấy số dư
+router.get("/check", async function(req, res, next) {
+  try {
+    const user_id= req.session.user.id; // Lấy userId từ session
+    const result = await healthy.query("SELECT * FROM wallet WHERE wallet_id = $1", [user_id]);
+    if (result.rows.length === 0) {
+      return res.status(404).json({ message: "Payment not found" });
+    }
+    res.status(200).json(result.rows);
+  } catch (error) {
+    console.error("Error in GET /api/payment/check:", error);
+    return res.status(500).json({ message: "Internal server error" });
+  }
+})
 module.exports = router;
